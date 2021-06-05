86d6c4ec40296a710e16ae147b1664ca2dfac556
hannun <eungoam12@gmail.com>
2021-06-06 01:42:43 +0900

    first commit

---
+++ 07format4.py
@@ -0,0 +1,17 @@
+output_a = "{:f}".format(52.273)
+output_b = "{:15f}".format(52.273)
+output_c = "{:+15f}".format(52.273)
+output_d = "{:+015f}".format(52.273)
+output_e = "{:15.3f}".format(52.273)
+output_f = "{:15.2f}".format(52.273)
+output_g = "{:15.1f}".format(52.273)
+
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+print(output_e)
+print(output_f)
+print(output_g)
+
